f69be4daf56c9b40fe6df25a00e8d6cc24bda051
xrozl <s1F102200064@toyo.jp>
2022-05-12 12:16:35 +0900

    Initial commit

---
+++ tut1/tutorial1.nim
@@ -0,0 +1,3 @@
+echo "What is your name?"
+var name: string = readLine(stdin)
+echo "Hello, ", name, "!"
---
+++ tut1/tutorial2.nim
@@ -0,0 +1,7 @@
+import strutils
+
+echo "calc test"
+var x, y: int
+x = parseInt(readLine(stdin))
+y = parseInt(readLine(stdin))
+echo "x * y = ",  x * y
